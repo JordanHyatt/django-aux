48c41ccc7c8644d23de82e20151eb934fb4bd83d
Jordan Hyatt <jordan.t.hyatt@gmail.com>
2022-07-06 09:00:00 -0400

    added decorators

---
+++ django_aux/decorators.py
@@ -0,0 +1,15 @@
+
+def logtest(logger_method):
+    def wrap(test_method):
+        def inner(instance):
+            logger_method.info(f'Running {test_method.__name__}')
+            test_method(instance)
+        return inner
+    return wrap
+
+
+def logtestclass(logger_method):
+    def wrap(TestClass):
+        logger_method(f'Imported {TestClass.__name__}')
+        return TestClass
+    return wrap
