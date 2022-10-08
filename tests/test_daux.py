4031618ce20b8ba8c51c267ba8c659ce8a514992
Jordan Hyatt <jordan.t.hyatt@gmail.com>
2022-10-08 12:30:28 -0400

    Finished Unit tests for SaveFilterMixin

--- tests/test_daux.py
+++ tests/test_daux.py
@@ -2,6 +2,7 @@ from django.test import TestCase, RequestFactory, Client
 from django_aux.models import *
 from .models import Person
 from .views import PersonLookup
+from .filters import PersonFilter
 from django.contrib.auth.models import User
 
 #------------Django-Aux TESTS------------
@@ -18,8 +19,7 @@ class TestSaveFilterMixin(TestCase):
         self.client.login(username='billy', password='password')
         
 
-    def test_savefiltermixin(self):
-        ''' Test functionality of save filter mixin '''
+    def test_savefiltermixin_get(self):
         response = self.client.get('/person-lookup')
         self.assertIsNone(response.wsgi_request.session.get('PersonLookup_qstr'))
         response = self.client.get('/person-lookup', {'last_name__icontains': 'hyatt', 'first_name__icontains':'jordan'})
@@ -30,4 +30,11 @@ class TestSaveFilterMixin(TestCase):
         self.assertEqual(qstr, 'last_name__icontains=hyatt&first_name__icontains=jordan')
         response = self.client.get('/person-lookup', {'clear_filter': 'Clear Filter'})
         qstr = response.wsgi_request.session.get('PersonLookup_qstr')
-        print(qstr, 'BLAHHH')
+    
+    def test_clear_filter(self):
+        request = self.factory.get('/person-lookup', {'last_name__icontains': 'hyatt', 'clear_filter': 'Clear Filter'})
+        request.user = self.user
+        view = PersonLookup()
+        view.setup(request)
+        kwargs = view.get_filterset_kwargs(PersonFilter)
+        self.assertEqual(kwargs, {})
